ec3ee21ccebc16d799d1e998c8f94684c731087d
Norald Alejo <norald.alejo@gmail.com>
2016-09-13 09:13:36 -0700

    Added an interface to keep track of miles traveled. Deleted interface to update mileage

--- Car.java
+++ Car.java
@@ -7,18 +7,24 @@ Date Created: September 12, 2016
 
 public interface Car{
 
-	//Speeds up object Car
-	public void speedUp(int num);
+	//Speeds up object Car. Calculates miles at acceleration
+	public void speedUp(int num, int miles);
 
-	//Slows down object Car
-	public void slowDown(int num);
+	//Slows down object Car. Calculates miles at deceleration
+	public void slowDown(int num, int miles);
 
-	//Adds miles to current mileage
-	public void carMiles(int miles);
+	//Calculates miles at constant speed
+	public void constantSpeed(int miles);
+
+	//Calculates the average speed
+	public void avgSpeed();
 
 	//Checks if gas is almost empty
 	public boolean checkGas(int gas);
 
 	//Gas decreases as car is used
 	public int gasTank(int gas);
+
+	//Prints miles traveled, new mileage, average speed, and tank level
+	public void print();
 }
