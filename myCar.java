62871d884609c88772ed00fbd9d142f9d0e93ec1
Norald Alejo <norald.alejo@gmail.com>
2016-09-12 20:32:49 -0700

    Added Subaru owner, year, model, current miles, and gas of car

--- myCar.java
+++ myCar.java
@@ -2,6 +2,6 @@ public class myCar{
 	public static void main(String [] args){
 
 		//Initializes Subaru Object
-		Subaru myFirstCar = new Subaru();
+		Subaru myFirstCar = new Subaru("Norald Alejo", 2002, "WRX", 242000, 22);
 	}
 }
