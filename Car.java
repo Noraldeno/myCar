8a8c619f6f6aa272a61ce8996bc1f0443fa87ed7
Norald Alejo <norald.alejo@gmail.com>
2016-09-13 16:32:44 -0700

    Added javadoc to interface

--- Car.java
+++ Car.java
@@ -1,30 +1,65 @@
-/*
-Title: Car Interface
-Name: Norald Alejo
-Date Created: September 12, 2016
-*/
-
+/**
+ * This program 
+ * @author  Norald Alejo
+ * @version 1.00
+ * 
+ */
 
 public interface Car{
 
-	//Speeds up object Car. Calculates miles at acceleration
+	/**
+	 * Accelerates car while calculating miles and time 
+	 * traveled
+	 * 
+	 * @param num 			The speed to accelerate
+	 * @param miles 		The miles traveled while accelerating
+	 * @param time			The time spent while accelerating
+	 */
 	public void speedUp(int num, int miles, int time);
 
-	//Slows down object Car. Calculates miles at deceleration
+	/**
+	 * Decelerates car while calculating miels and time traveled
+	 * 
+	 * @param num 			The speed to decelerate
+	 * @param miles 		The miles traveled while decelerating
+	 * @param time			The time spent while decelerating
+	 */
 	public void slowDown(int num, int miles, int time);
 
-	//Calculates miles at constant speed
+	/**
+	 * Calcuates miles traveled in a time while traveling at constant speed
+	 * 
+	 * @param miles 		The miles traveled 
+	 * @param time			The time spent driving
+	 */
 	public void constantSpeed(int miles, int time);
 
-	//Calculates the average speed
+	/**
+	 * Calculates the average speed in one drive
+	 * 
+	 * @return time/hour 	The calculation of distance over a set time
+	 */
 	public float avgSpeed();
 
-	//Checks if gas is almost empty
+	/**
+	 * Checks if tank is empty.
+	 * 
+	 * @param gas 			The amount of gas used
+	 * @return true			Returns true if gas is not empty
+	 * @return false 		Returns false if gas is empty
+	 */
 	public boolean checkGas(int gas);
 
-	//Gas decreases as car is used
+	/**
+	 * Calculates how much gas is left in tank
+	 * 
+	 * @param gas 			The amount of gas that is being used
+	 * @return tank 		The amount of gas left in tank
+	 */
 	public float gasTank(int gas);
 
-	//Prints miles traveled, new mileage, average speed, and tank level
+	/**
+	 * Prints miles traveled, new mileage, average speed, and tank level
+	 */
 	public void print();
 }
