776cad3088cc27bb24185c086de904874b46317f
Norald Alejo <norald.alejo@gmail.com>
2016-09-12 20:10:58 -0700

    Added interface to slow down. Added gas interface

--- Car.java
+++ Car.java
@@ -9,4 +9,19 @@ public interface Car{
 
 	//Speeds up object Car
 	public void speedUp(int num);
+
+	//Slows down object Car
+	public void slowDown(int num);
+
+	//Adds miles to current mileage
+	public void addMileage(int miles);
+
+	//Takes in gas per mile
+	public void gasMileage(int gas);
+
+	//Checks if gas is almost empty
+	public boolean checkGas(int gas);
+
+	//Gas decreases as car is used
+	public int gasTank(int gas);
 }
