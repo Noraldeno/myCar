ccfb19c36cc2ccc89b4765c28ab6b3212ad92fb8
Norald Alejo <norald.alejo@gmail.com>
2016-09-13 09:24:20 -0700

    Implemented interface to keep track of time and miles traveled. Implemented interface to calculate average speed. Changed some names in Subaru objects

--- Subaru.java
+++ Subaru.java
@@ -8,38 +8,46 @@ Date Created: September 12, 2016
 public class Subaru implements Car{
 	//Initializes speed
 	private int speed = 0;
+	private int travel = 0;
 
 	private String owner, model;
-	private int year, carMiles, gasMileage;
+	private int year, car, mileage, time, fuelEcon;
 
 	public Subaru(String owner, int year, String model, int miles, int gas){
 		this.owner = owner;
 		this.year = year;
 		this.model = model;
-		carMiles = miles;
-		gasMileage = gas;
-
-		System.out.printf("Make: Subaru\nYear: %d\nModel: %s\nOwner: %s\nCurrent Miles: %d\nMileage: %d", year, model, owner, carMiles, gasMileage);
+		mileage = miles;
+		fuelEcon = gas;
+		
+		System.out.printf("Make: Subaru\nYear: %d\nModel: %s\nOwner: %s\nCurrent Miles: %d\nMileage: %d", year, model, owner, mileage, fuelEcon);
 	}
 
 	//Accelerates car
-	public void speedUp(int num){
+	public void speedUp(int num, int miles, int hour){
 		speed += num;
+		travel += miles;
+		time += hour;
 		System.out.println("Your current speed is now " + num);
 	}
 
 	//Decelerates car
-	public void slowDown(int num){
+	public void slowDown(int num, int miles, int hour){
 		speed -= num;
+		travel += miles;
+		time += hour;
 		System.out.println("Your current speed is now " + num);
 	}
 
-	public void carMiles(int miles){
-		carMiles += miles;
+	public void constantSpeed(int miles, int hour){
+		travel += miles;
+		time += hour;
 	}
 
-	
-
+	//Calculates average speed
+	public int avgSpeed(){
+		return travel/time;
+	}
 	//Checks if gas is almost empty
 	public boolean checkGas(int gas){
 		return false;
@@ -48,5 +56,9 @@ public class Subaru implements Car{
 	//Gas decreases as car is used
 	public int gasTank(int gas){
 		return 0;
+	}
+
+	public void print(){
+		System.out.println();
 	}
 }
