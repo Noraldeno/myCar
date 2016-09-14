bf11438794ca31864d2328ee89cfee48c1f9ff67
Norald Alejo <norald.alejo@gmail.com>
2016-09-13 18:06:18 -0700

    Added javadoc

--- myCar.java
+++ myCar.java
@@ -1,7 +1,18 @@
+/**
+ * This program allows object Subaru to interact with outside world
+ * 
+ * @author  Norald Alejo
+ * @version 1.00
+ * 
+ */
+
+
 public class myCar{
 	public static void main(String [] args){
 
 		//Initializes Subaru Object
-		Subaru myFirstCar = new Subaru("Norald Alejo", 2002, "WRX", 242000, 22);
+		Subaru myFirstCar = new Subaru("Norald Alejo", 2002, "WRX", 242000, 22, 12);
+
+		myFirstCar.speedUp(23, 5, 4);
 	}
 }
